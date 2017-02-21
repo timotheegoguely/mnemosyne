5b23b3795e016d1d972bfd89f2f1006e3b271a93
Akram Dhane <akram.dhane@gmail.com>
2017-02-21 14:56:40 +0100

    db relations & validations verified

--- app/models/diploma.rb
+++ app/models/diploma.rb
@@ -1,10 +1,10 @@
 class Diploma < ApplicationRecord
-  has_many :thesis
+  has_many :theses
   has_many :school_diplomas
   has_many :schools, through: :school_diplomas
   has_many :diploma_subcategories
   has_many :subcategories, through: :diploma_subcategories
-
+  # Validations
   validates :name, presence: true
   validates :degree, presence: true
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,9 +1,9 @@
 class Subcategory < ApplicationRecord
   belongs_to :category
   has_many :school_subcategories
-  has_many :schools, through: :school_subcategories
   has_many :diploma_subcategories
+  has_many :schools, through: :school_subcategories
   has_many :diplomas, through: :diploma_subcategories
-
+  # Validations
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
