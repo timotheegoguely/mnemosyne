6435f944485f48998e534c3be6820c659043e92a
Akram Dhane <akram.dhane@gmail.com>
2017-02-21 17:03:05 +0100

    Thesis View

--- app/controllers/theses_controller.rb
+++ app/controllers/theses_controller.rb
@@ -1,4 +1,6 @@
 class ThesesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def index
+    @theses = policy_scope(Thesis).order(created_at: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   scope '(:locale)', locale: /fr|en/ do
-    root to: 'pages#home'
+    root to: 'theses#index' # pages#home
     resources :users do
       resources :theses
     end
     resources :theses, only: :index
-    resources :schools, only: [ :index, :show ]
+    resources :schools, only: [ :index, :show, :edit, :update ]
   end
 end
