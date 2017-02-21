bb919fd7dbea9380030dcfe3e27d5047de74fcc9
Richard Phonthibsvads <phs.richards@gmail.com>
2017-02-21 16:26:57 +0100

    Create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   get 'schools/index'
 
   get 'schools/show'
