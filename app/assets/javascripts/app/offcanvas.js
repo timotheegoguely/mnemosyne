f274cc1e324639bec10e40550d6e1b340050e2f4
Timothee Goguely <timotheegoguely@gmail.com>
2017-02-22 12:04:20 +0100

    Add filters buttons and update theses index

---
+++ app/assets/javascripts/app/offcanvas.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+  $( '#offcanvas-left' ).offcanvas({
+    // push: true, // boolean
+    width: '150px', // string
+    // direction: 'left', // string: left or right
+    // toggleEvent: 'click',
+    // clickOutside: true, // boolean
+  });
+});
