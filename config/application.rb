52b0273d07d476ab04356e7713072c5e8dcbbd8b
Akram Dhane <akram.dhane@gmail.com>
2017-02-23 16:21:25 +0100

    bookmark button in thesis show operational

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Mnemosyne
 
     config.i18n.default_locale = :fr
 
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
