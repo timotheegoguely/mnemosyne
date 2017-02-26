5c19be72b2effb543f94bc7e78b56c0c0e1e6203
Timothee Goguely <timotheegoguely@gmail.com>
2017-02-26 23:49:48 +0100

    Update navbar responsivness, map, messages and layouts

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -2,6 +2,7 @@ class Devise::RegistrationsController < DeviseController
   prepend_before_action :require_no_authentication, only: [:new, :create, :cancel]
   prepend_before_action :authenticate_scope!, only: [:edit, :update, :destroy]
   prepend_before_action :set_minimum_password_length, only: [:new, :edit]
+  layout "basic"
 
   # GET /resource/sign_up
   def new
