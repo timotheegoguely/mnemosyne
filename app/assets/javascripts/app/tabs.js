d6e48982fa4d4e0b661a7051f49b81ba5b9843d3
Timothee Goguely <timotheegoguely@gmail.com>
2017-02-28 17:08:43 +0100

    mapbox + pop styling

--- app/assets/javascripts/app/tabs.js
+++ app/assets/javascripts/app/tabs.js
@@ -3,6 +3,9 @@ $( document ).ready( function() {
   setTimeout( function(){
     map.invalidateSize();
   }, 200);
+  setTimeout( function(){
+    map.invalidateSize();
+  }, 1000);
 
   if ($( '#tab-cards' ).hasClass( 'active' )) {
     $( '#card-preview' ).show();
