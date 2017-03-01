6bfea51dba595ce19e3e8f877f366b79c7b74baa
Richard Phonthibsvads <phs.richards@gmail.com>
2017-03-01 12:01:54 +0100

    Escape Thesis show to index with ESC

--- app/assets/javascripts/app/utilities.js
+++ app/assets/javascripts/app/utilities.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
   $(document).keydown(function(e) {
-    if (e.keyCode == 37) {
+    e.preventDefault();
+    if (e.keyCode == 37) { // left
       $('#thesis-previous').trigger('click');
     }
-    else if (e.keyCode == 39) {
+    else if (e.keyCode == 39) { //right
       $('#thesis-next').trigger('click');
+    }
+    else if (e.keyCode == 27) { // esc
+      if ($('#thesis-content-wrapper').length) {
+        parent.history.back();
+        return false;
+      }
     }
   });
 })
