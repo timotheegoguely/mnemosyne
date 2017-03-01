8067a1fb208a32232249c53880da11b89b9491f6
laura crief <crieflaura@gmail.com>
2017-03-01 18:59:46 +0100

    drop and data

--- app/assets/javascripts/app/dropzone_callback.js
+++ app/assets/javascripts/app/dropzone_callback.js
@@ -1,7 +1,14 @@
-// $(function() {
-//   var myDropzone = new Dropzone("#new_thesis");
-//   myDropzone.on("success", function(file, json) {
-//     console.log(json);
-//     // injecter la preview + les data dans le formulaire + les tags
-//   });
-// })
+Dropzone.autoDiscover = false; // keep me OUTSIDE a document ready
+
+$(function() {
+  var myDropzone = new Dropzone("#new_thesis");
+
+  myDropzone.on("success", function(file, json) {
+    console.log(json);
+
+    $("#thesis_title").val(json["infos"]["Title"]);
+    $("#thesis_tag_list").val(json["infos"]["Keywords"]);
+
+    // injecter la preview + les data dans le formulaire + les tags
+  });
+});
