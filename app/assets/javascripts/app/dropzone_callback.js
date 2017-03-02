6a3e19f30bb44a9f69f418e8289b85dbd0d97b51
Timothee Goguely <timotheegoguely@gmail.com>
2017-03-02 13:44:08 +0100

    dropzone positioning

--- app/assets/javascripts/app/dropzone_callback.js
+++ app/assets/javascripts/app/dropzone_callback.js
@@ -4,11 +4,12 @@ $(function() {
   var myDropzone = new Dropzone("#new_thesis");
 
   myDropzone.on("success", function(file, json) {
-    // console.log(json);
+    console.log(json);
 
     var jsonValue = json["infos"]["Keywords"];
 
     $("#thesis_title").val(json["infos"]["Title"]);
+    $("#thesis_resume").val(json["infos"]["Subject"]);
     $("#thesis_tag_list").val(jsonValue);
 
     var tagValue = $("#thesis_tag_list").val();
