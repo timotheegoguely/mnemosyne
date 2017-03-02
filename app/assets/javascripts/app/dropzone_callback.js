bde3bad193df0525da80f2fda15f0664f7152df1
laura crief <crieflaura@gmail.com>
2017-03-02 11:26:07 +0100

    remplace caractères dans la list de tag

--- app/assets/javascripts/app/dropzone_callback.js
+++ app/assets/javascripts/app/dropzone_callback.js
@@ -6,9 +6,17 @@ $(function() {
   myDropzone.on("success", function(file, json) {
     console.log(json);
 
+    var jsonValue = json["infos"]["Keywords"];
+
     $("#thesis_title").val(json["infos"]["Title"]);
-    $("#thesis_tag_list").val(json["infos"]["Keywords"]);
+    $("#thesis_tag_list").val(jsonValue);
+
+    var tagValue = $("#thesis_tag_list").val();
+    var newValue = tagValue.replace(/;/g, ",");
+
+     $("#thesis_tag_list").val(newValue);
 
     // injecter la preview + les data dans le formulaire + les tags
   });
 });
+
